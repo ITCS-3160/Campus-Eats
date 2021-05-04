5f5a50e8d2eb20893aa2417d81bcb147c29f2e18
Kris Bowen <81261100+kbowen26@users.noreply.github.com>
2021-05-04 15:06:42 -0400

    Advanced Views and Screenshots/Tables
    Updated to include Explain statements

--- AdvancedView1_Team1.sql
+++ AdvancedView1_Team1.sql
@@ -1,5 +1,14 @@
 /* View the on time ratings of drivers with a courteous rating of 3 or 
 more */
+-- Explain 
+EXPLAIN SELECT rating_id, courteous_rating, onTime_rating
+FROM (
+SELECT rating_id, courteous_rating, onTime_rating
+FROM driver_rating
+WHERE courteous_rating >= 3) AS courteous_3
+ORDER BY onTime_rating;
+
+
 -- Select Rating IDs, Courteous ratings, and the On Time ratings of drivers
 SELECT rating_id, courteous_rating, onTime_rating
 
--- AdvancedView2_Team1.sql
+++ AdvancedView2_Team1.sql
@@ -1,5 +1,15 @@
 /* View restaurant ratings and compare overall to the average price and 
 food ratings.*/
+-- Explain
+EXPLAIN SELECT rating_id, food_rating, price_rating, overall_rating, food_price_sum/2 AS food_price_avg_rating
+FROM (SELECT rating.rating_id, food_rating, price_rating, overall_rating,
+		food_rating + price_rating AS food_price_sum
+		FROM restaurant_ratings
+        -- Join rating and restaurant_ratings by rating_id
+        INNER JOIN rating
+		ON rating.rating_id = restaurant_ratings.rating_id) AS sub
+ORDER BY overall_rating;
+
 -- Select rating IDs, food ratings, price ratings, and overall ratings of restaurants
 SELECT rating_id, food_rating, price_rating, overall_rating, food_price_sum/2 AS food_price_avg_rating
 
@@ -13,4 +23,3 @@ FROM (SELECT rating.rating_id, food_rating, price_rating, overall_rating,
 
 -- Order by overall rating
 ORDER BY overall_rating;
-
